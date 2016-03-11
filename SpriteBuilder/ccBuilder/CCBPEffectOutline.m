oxeron/SpriteBuilder

commit 070acd0895c6202900008a030f93868557033d9a

Make SpriteBuilder 1.5 compile and use cocos2d-objc 3.4.9

SpriteBuilder/ccBuilder/CCBPEffectOutline.m
@@ -7,9 +7,13 @@
 //
 
 #import "CCBPEffectOutline.h"
+
+
+#if CC_EFFECTS_EXPERIMENTAL
+
 #import "EffectsUndoHelper.h"
-#import "CCBReaderInternal.h"
-#import "CCBWriterInternal.h"
+#importCCBDictionaryReader
+#importCCBDictionaryWriter
 
 
 @implementation CCBPEffectOutline
@@ -23,7 +27,7 @@
 -(id)serialize
 {
     return @[SERIALIZE_PROPERTY(outlineWidth,Integer),
-             @{@"name" : @"outlineColor", @"type" : @"Color4", @"value": [CCBWriterInternal serializeColor4:self.outlineColor] },
+             @{@"name" : @"outlineColor", @"type" : @"Color4", @"value": [CCBDictionaryWriter serializeColor4:self.outlineColor] },
              ];
 }
 
@@ -35,9 +39,9 @@
         return [dict[@"name"] isEqualToString:@"outlineColor"];\
     } complete:^(NSDictionary * dict, int idx) {
         
-        self.outlineColor = [CCBReaderInternal deserializeColor4:dict[@"value"]];
+        self.outlineColor = [CCBDictionaryReader deserializeColor4:dict[@"value"]];
     }];
-
+    
 }
 
 -(EffectDescription*)effectDescription
@@ -52,3 +56,5 @@
 }
 
 @end
+
+#endif

SpriteBuilder/ccBuilder/CCBFileUtil.m
@@ -165,7 +165,7 @@
 							 @"external/Chipmunk/doc",
 							 @"external/Chipmunk/doc-src",
 							 @"external/Chipmunk/msvc",
-							 @"external/Chipmunk/xcode",
+							 //@"external/Chipmunk/xcode",
 							 @"external/ObjectAL/ObjectAL/diagrams",
 							 @"external/ObjectAL/ObjectALDemo",
 							 @"external/ObjectAL/Sample Code",

SpriteBuilder/ccBuilder/CCBPEffectOutline.h
@@ -7,8 +7,13 @@
 //
 
 #import "CCEffectOutline.h"
+
+#if CC_EFFECTS_EXPERIMENTAL
+
 #import "EffectsManager.h"
 
 @interface CCBPEffectOutline : CCEffectOutline<EffectProtocol>
 
 @end
+
+#endif

SpriteBuilder/ccBuilder/EffectOutlineControl.h
@@ -7,6 +7,9 @@
 //
 
 #import "EffectViewController.h"
+
+#if CC_EFFECTS_EXPERIMENTAL
+
 #import "CCBPEffectOutline.h"
 
 @interface EffectOutlineControl : EffectViewController
@@ -15,3 +18,5 @@
 @property (nonatomic) CCBPEffectOutline * effect;
 
 @end
+
+#endif

SpriteBuilder/ccBuilder/EffectOutlineControl.m
@@ -8,6 +8,8 @@
 
 #import "EffectOutlineControl.h"
 
+#if CC_EFFECTS_EXPERIMENTAL
+
 @interface EffectOutlineControl ()
 
 @end
@@ -41,3 +43,6 @@
 }
 
 @end
+
+#endif
+

SpriteBuilder/ccBuilder/ImageAndTextCell.m
@@ -60,17 +60,17 @@ static CGFloat IMAGE_PADDING_RIGHT = 3.0;
 - copyWithZone:(NSZone *)zone
 {
     ImageAndTextCell *cell = (ImageAndTextCell *)[super copyWithZone:zone];
-    cell.image = _image;
-    cell.imageAlt = _imageAlt;
+    cell.image = self.image;
+    cell.imageAlt = self.imageAlt;
     return cell;
 }
 
 - (NSRect)imageAltFrameForCellFrame:(NSRect)cellFrame
 {
-    if (_imageAlt != nil)
+    if (self.imageAlt != nil)
 	{
         NSRect imageFrame;
-        imageFrame.size = [_imageAlt size];
+        imageFrame.size = [self.imageAlt size];
         imageFrame.origin = cellFrame.origin;
         imageFrame.origin.x += cellFrame.size.width - 14.0;
         imageFrame.origin.y += ceil((cellFrame.size.height - imageFrame.size.height) / 2);
@@ -85,14 +85,14 @@ static CGFloat IMAGE_PADDING_RIGHT = 3.0;
 - (void)editWithFrame:(NSRect)aRect inView:(NSView *)controlView editor:(NSText *)textObj delegate:(id)anObject event:(NSEvent *)theEvent
 {
     NSRect titleRect = [self titleRectForBounds:aRect];
-    titleRect.origin.x += [_image size].width;
+    titleRect.origin.x += [self.image size].width;
     [super editWithFrame:titleRect inView:controlView editor:textObj delegate:anObject event:theEvent];
 }
 
 - (void)selectWithFrame:(NSRect)aRect inView:(NSView *)controlView editor:(NSText *)textObj delegate:(id)anObject start:(NSInteger)selStart length:(NSInteger)selLength
 {
     NSRect titleRect = [self titleRectForBounds:aRect];
-    titleRect.origin.x += [_image size].width;
+    titleRect.origin.x += [self.image size].width;
     [super selectWithFrame:titleRect inView:controlView editor:textObj delegate:anObject start:selStart length:selLength];
 }
 
@@ -106,23 +106,23 @@ static CGFloat IMAGE_PADDING_RIGHT = 3.0;
 
 - (void)drawImageAlt:(NSRect)cellFrame controlView:(NSView *)controlView
 {
-    if (_imageAlt == nil)
+    if (self.imageAlt == nil)
     {
         return;
     }
 
-    [_imageAlt setFlipped:[controlView isFlipped]];
-    [_imageAlt drawAtPoint:[self imageAltFrameForCellFrame:cellFrame].origin fromRect:NSZeroRect operation:NSCompositeSourceOver fraction:1.0];
+    [self.imageAlt setFlipped:[controlView isFlipped]];
+    [self.imageAlt drawAtPoint:[self imageAltFrameForCellFrame:cellFrame].origin fromRect:NSZeroRect operation:NSCompositeSourceOver fraction:1.0];
 }
 
 - (void)drawImage:(NSRect *)cellFrame controlView:(NSView *)controlView
 {
-    if (_image == nil)
+    if (self.image == nil)
 	{
         return;
     }
 
-    NSSize imageSize = [_image size];
+    NSSize imageSize = [self.image size];
     NSRect imageFrame;
     NSDivideRect((*cellFrame), &imageFrame, cellFrame, IMAGE_PADDING_RIGHT + imageSize.width, NSMinXEdge);
 
@@ -135,8 +135,8 @@ static CGFloat IMAGE_PADDING_RIGHT = 3.0;
     imageFrame.origin.x += IMAGE_PADDING_RIGHT;
     imageFrame.origin.y += 2;
 
-    [_image setFlipped:[controlView isFlipped]];
-    [_image drawAtPoint:imageFrame.origin fromRect:NSZeroRect operation:NSCompositeSourceOver fraction:1.0];
+    [self.image setFlipped:[controlView isFlipped]];
+    [self.image drawAtPoint:imageFrame.origin fromRect:NSZeroRect operation:NSCompositeSourceOver fraction:1.0];
 }
 
 - (NSRect)titleRectForBounds:(NSRect)theRect
@@ -182,7 +182,7 @@ static CGFloat IMAGE_PADDING_RIGHT = 3.0;
 - (NSSize)cellSize
 {
     NSSize cellSize = [super cellSize];
-    cellSize.width += (_image ? [_image size].width : 0) + IMAGE_PADDING_RIGHT;
+    cellSize.width += (self.image ? [self.image size].width : 0) + IMAGE_PADDING_RIGHT;
     return cellSize;
 }
 

Support/PROJECTNAME.spritebuilder/PROJECTNAME.xcodeproj/project.pbxproj
@@ -122,26 +122,12 @@
 			remoteGlobalIDString = D2FEB74F194F6C9E00FC0574;
 			remoteInfo = cocos2dAndroid;
 		};
-		7A97910119E62E40001FFC4D /* PBXContainerItemProxy */ = {
-			isa = PBXContainerItemProxy;
-			containerPortal = B7378940180761A80076A88C /* cocos2d.xcodeproj */;
-			proxyType = 2;
-			remoteGlobalIDString = 5BF3267A195F8D8800D9A51A;
-			remoteInfo = cocos2dJava;
-		};
 		7A97910319E62E40001FFC4D /* PBXContainerItemProxy */ = {
 			isa = PBXContainerItemProxy;
 			containerPortal = B7378940180761A80076A88C /* cocos2d.xcodeproj */;
 			proxyType = 2;
 			remoteGlobalIDString = 7A4037A819E37038007B6E8F;
 			remoteInfo = "cocos2d-mac";
-		};
-		7A97910519E62E4C001FFC4D /* PBXContainerItemProxy */ = {
-			isa = PBXContainerItemProxy;
-			containerPortal = B7378940180761A80076A88C /* cocos2d.xcodeproj */;
-			proxyType = 1;
-			remoteGlobalIDString = 5BF32679195F8D8800D9A51A;
-			remoteInfo = cocos2dJava;
 		};
 		7A97910F19E6319A001FFC4D /* PBXContainerItemProxy */ = {
 			isa = PBXContainerItemProxy;
@@ -157,19 +143,19 @@
 			remoteGlobalIDString = 7A4037A719E37038007B6E8F;
 			remoteInfo = "cocos2d-mac";
 		};
-		7A97911F19E63219001FFC4D /* PBXContainerItemProxy */ = {
-			isa = PBXContainerItemProxy;
-			containerPortal = B7378940180761A80076A88C /* cocos2d.xcodeproj */;
-			proxyType = 1;
-			remoteGlobalIDString = 5BF32679195F8D8800D9A51A;
-			remoteInfo = cocos2dJava;
-		};
 		7A97912119E6321C001FFC4D /* PBXContainerItemProxy */ = {
 			isa = PBXContainerItemProxy;
 			containerPortal = B7378940180761A80076A88C /* cocos2d.xcodeproj */;
 			proxyType = 1;
 			remoteGlobalIDString = D2FEB60D194F6C9E00FC0574;
 			remoteInfo = cocos2dAndroid;
+		};
+		FF62F7B11C897AB5004302E1 /* PBXContainerItemProxy */ = {
+			isa = PBXContainerItemProxy;
+			containerPortal = B7378940180761A80076A88C /* cocos2d.xcodeproj */;
+			proxyType = 2;
+			remoteGlobalIDString = B7E7DE341A76DB7D004234B7;
+			remoteInfo = cocos2dMacFramework;
 		};
 /* End PBXContainerItemProxy section */
 
@@ -181,7 +167,7 @@
 		5B121013197483EE004C1E1D /* ic_launcher.png */ = {isa = PBXFileReference; lastKnownFileType = image.png; path = ic_launcher.png; sourceTree = "<group>"; };
 		5B121015197483EE004C1E1D /* ic_launcher.png */ = {isa = PBXFileReference; lastKnownFileType = image.png; path = ic_launcher.png; sourceTree = "<group>"; };
 		5B12101B19748413004C1E1D /* PROJECTIDENTIFIERActivity.java */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.java; name = PROJECTIDENTIFIERActivity.java; path = Source/Platforms/Android/java/org/cocos2d/PROJECTIDENTIFIER/PROJECTIDENTIFIERActivity.java; sourceTree = "<group>"; };
-		5B121021197484A3004C1E1D /* PROJECTNAME Java.jar */ = {isa = PBXFileReference; explicitFileType = compiled.java.jar; includeInIndex = 0; path = "PROJECTNAME Java.jar"; sourceTree = BUILT_PRODUCTS_DIR; };
+		5B121021197484A3004C1E1D /* PROJECTNAME Java.jar */ = {isa = PBXFileReference; includeInIndex = 0; lastKnownFileType = archive.jar; path = "PROJECTNAME Java.jar"; sourceTree = BUILT_PRODUCTS_DIR; };
 		5B12102E197484FA004C1E1D /* PROJECTIDENTIFIERActivity.h */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.h; name = PROJECTIDENTIFIERActivity.h; path = Source/Platforms/Android/PROJECTIDENTIFIERActivity.h; sourceTree = "<group>"; };
 		5B12102F197484FA004C1E1D /* PROJECTIDENTIFIERActivity.m */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.objc; name = PROJECTIDENTIFIERActivity.m; path = Source/Platforms/Android/PROJECTIDENTIFIERActivity.m; sourceTree = "<group>"; };
 		5B121033197485FE004C1E1D /* Info.plist */ = {isa = PBXFileReference; lastKnownFileType = text.plist.xml; name = Info.plist; path = Platforms/Android/Info.plist; sourceTree = "<group>"; };
@@ -448,8 +434,8 @@
 			children = (
 				7A9790FE19E62E40001FFC4D /* libcocos2d.a */,
 				7A97910019E62E40001FFC4D /* libcocos2dAndroid.a */,
-				7A97910219E62E40001FFC4D /* cocos2dJava.jar */,
 				7A97910419E62E40001FFC4D /* libcocos2d-mac.a */,
+				FF62F7B21C897AB5004302E1 /* cocos2dMacFramework.framework */,
 			);
 			name = Products;
 			sourceTree = "<group>";
@@ -593,7 +579,6 @@
 			buildRules = (
 			);
 			dependencies = (
-				7A97910619E62E4C001FFC4D /* PBXTargetDependency */,
 			);
 			name = "PROJECTNAME Java";
 			productName = "PROJECTNAME Java";
@@ -631,7 +616,6 @@
 			);
 			dependencies = (
 				7A97912219E6321C001FFC4D /* PBXTargetDependency */,
-				7A97912019E63219001FFC4D /* PBXTargetDependency */,
 				5B121032197485D1004C1E1D /* PBXTargetDependency */,
 			);
 			name = "PROJECTNAME Android";
@@ -665,6 +649,7 @@
 		B77F1B2617B978D7009739AE /* Project object */ = {
 			isa = PBXProject;
 			attributes = {
+				LastSwiftUpdateCheck = 0720;
 				LastUpgradeCheck = 0610;
 				ORGANIZATIONNAME = Apportable;
 				TargetAttributes = {
@@ -722,18 +707,18 @@
 			remoteRef = 7A9790FF19E62E40001FFC4D /* PBXContainerItemProxy */;
 			sourceTree = BUILT_PRODUCTS_DIR;
 		};
-		7A97910219E62E40001FFC4D /* cocos2dJava.jar */ = {
-			isa = PBXReferenceProxy;
-			fileType = archive.jar;
-			path = cocos2dJava.jar;
-			remoteRef = 7A97910119E62E40001FFC4D /* PBXContainerItemProxy */;
-			sourceTree = BUILT_PRODUCTS_DIR;
-		};
 		7A97910419E62E40001FFC4D /* libcocos2d-mac.a */ = {
 			isa = PBXReferenceProxy;
 			fileType = archive.ar;
 			path = "libcocos2d-mac.a";
 			remoteRef = 7A97910319E62E40001FFC4D /* PBXContainerItemProxy */;
+			sourceTree = BUILT_PRODUCTS_DIR;
+		};
+		FF62F7B21C897AB5004302E1 /* cocos2dMacFramework.framework */ = {
+			isa = PBXReferenceProxy;
+			fileType = wrapper.framework;
+			path = cocos2dMacFramework.framework;
+			remoteRef = FF62F7B11C897AB5004302E1 /* PBXContainerItemProxy */;
 			sourceTree = BUILT_PRODUCTS_DIR;
 		};
 /* End PBXReferenceProxy section */
@@ -901,11 +886,6 @@
 			target = 5B121020197484A3004C1E1D /* PROJECTNAME Java */;
 			targetProxy = 5B121031197485D1004C1E1D /* PBXContainerItemProxy */;
 		};
-		7A97910619E62E4C001FFC4D /* PBXTargetDependency */ = {
-			isa = PBXTargetDependency;
-			name = cocos2dJava;
-			targetProxy = 7A97910519E62E4C001FFC4D /* PBXContainerItemProxy */;
-		};
 		7A97911019E6319A001FFC4D /* PBXTargetDependency */ = {
 			isa = PBXTargetDependency;
 			name = "cocos2d-ios";
@@ -915,11 +895,6 @@
 			isa = PBXTargetDependency;
 			name = "cocos2d-mac";
 			targetProxy = 7A97911519E631A1001FFC4D /* PBXContainerItemProxy */;
-		};
-		7A97912019E63219001FFC4D /* PBXTargetDependency */ = {
-			isa = PBXTargetDependency;
-			name = cocos2dJava;
-			targetProxy = 7A97911F19E63219001FFC4D /* PBXContainerItemProxy */;
 		};
 		7A97912219E6321C001FFC4D /* PBXTargetDependency */ = {
 			isa = PBXTargetDependency;
@@ -1267,7 +1242,7 @@
 					"\"Source/libs/cocos2d-iphone/external/ObjectAL\"/**",
 				);
 				INFOPLIST_FILE = Source/Resources/Platforms/iOS/Info.plist;
-				IPHONEOS_DEPLOYMENT_TARGET = 7.0;
+				IPHONEOS_DEPLOYMENT_TARGET = 8.0;
 				LD_RUNPATH_SEARCH_PATHS = "$(inherited) @executable_path/Frameworks";
 				ONLY_ACTIVE_ARCH = YES;
 				OTHER_LDFLAGS = (
@@ -1301,7 +1276,7 @@
 					"\"Source/libs/cocos2d-iphone/external/ObjectAL\"/**",
 				);
 				INFOPLIST_FILE = Source/Resources/Platforms/iOS/Info.plist;
-				IPHONEOS_DEPLOYMENT_TARGET = 7.0;
+				IPHONEOS_DEPLOYMENT_TARGET = 8.0;
 				LD_RUNPATH_SEARCH_PATHS = "$(inherited) @executable_path/Frameworks";
 				OTHER_LDFLAGS = (
 					"-lz",

commit a644e2f59f7109246335058eb211b2711ee025cd

Correct an import bug introduced in 070acd0

SpriteBuilder/ccBuilder/CCBPEffectOutline.m
@@ -12,8 +12,8 @@
 #if CC_EFFECTS_EXPERIMENTAL
 
 #import "EffectsUndoHelper.h"
-#importCCBDictionaryReader
-#importCCBDictionaryWriter
+#import "CCBDictionaryReader.h"
+#import "CCBDictionaryWriter.h"
 
 
 @implementation CCBPEffectOutline